cf2ae8edd4569ef3ad9470aecfa0195cfe46a3c0
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2025-03-30 13:22:09 -0300

    commit de alterações

---
+++ src/test/java/SomaCalculadoraTeste.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SomaCalculadora {
+
+        @Test
+        public void testeSoma() {
+            Calculadora calculadora = new Calculadora();
+            int resultado = calculadora.soma(5, 3);
+            assertEquals(8, resultado);
+        }
+
+        @Test
+        public void testeSomaComNegativos() {
+            Calculadora calculadora = new Calculadora();
+            int resultado = calculadora.soma(-5, 3);
+            assertEquals(-2, resultado);
+        }
+    }
---
+++ src/test/java/SubtracaoCalculadoraTeste.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class SubtracaoCalculadora {
+    @Test
+    public void testeSubtracao{
+        Calculadora calculadora = new Calculadora();
+
+    }
+}
